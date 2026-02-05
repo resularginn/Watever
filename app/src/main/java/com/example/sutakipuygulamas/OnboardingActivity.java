24b21911804506abe1d444da87f36d0a3b343447
Resul Argın <110631882+resularginn@users.noreply.github.com>
2026-02-05 15:16:37 +0300

    Update OnboardingActivity.java

--- app/src/main/java/com/example/sutakipuygulamas/OnboardingActivity.java
+++ app/src/main/java/com/example/sutakipuygulamas/OnboardingActivity.java
@@ -11,20 +11,16 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 1. KONTROL: Kullanıcı zaten kayıtlı mı?
         SharedPreferences pref = getSharedPreferences("WateverData", MODE_PRIVATE);
         if (pref.getBoolean("isRegistered", false)) {
-            // Evet, kayıtlı. O zaman bu ekranı ve Login ekranını atla, direkt Main'e git.
             startActivity(new Intent(this, MainActivity.class));
             finish();
             return;
         }
 
-        // Kayıtlı değilse bu tatlı ekranı göster
         setContentView(R.layout.activity_onboarding);
 
         Button btnStart = findViewById(R.id.btn_start);
-        // OnboardingActivity içindeki buton tıklama olayı:
         btnStart.setOnClickListener(v -> {
             startActivity(new Intent(OnboardingActivity.this, SetupProfileActivity.class));
             finish();
